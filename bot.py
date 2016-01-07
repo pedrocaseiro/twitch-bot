6214d4e6f20c3c30db4f3fbd45f09d50c1539c2a
pedrocaseiro <pcaseiro96@gmail.com>
2016-01-07 22:10:03 +0000

    receive ping from server and answer

--- bot.py
+++ bot.py
@@ -1,3 +1,24 @@
+import cfg
+import socket
+
+s=socket.socket()
+s.connect((HOST,PORT))
+
+#send the oauthtocken, nick and channel to join
+s.send("PASS {}\r\n".format(PASS).encode("utf-8"))
+s.send("NICK {}\r\n".format(NICK).encode("utf-8"))
+s.send("JOIN {}\r\n".format(CHAN).encode("utf-8"))
+
+
+
+#receive ping from server and answer pong
+while True:
+	response = s.recv(1024).decode("utf-8")
+	if response == "PING :tmi.twitch.tv\r\n":
+		s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+	else:
+		print(response)
+
 def chat(sock, msg):
 	sock.send("PRIVMSG #{} :{}".format(cfg.CHAN,msg))
 
